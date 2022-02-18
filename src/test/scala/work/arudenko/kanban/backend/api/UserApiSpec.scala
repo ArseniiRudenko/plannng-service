5fae5ab61592baaa0d3794a72948cd772d324bc0
arsenii <ruenkoarseniq@gmal.com>
2022-02-18 19:35:39 +0100

    added spec for user api

--- src/test/scala/work/arudenko/kanban/backend/api/UserApiSpec.scala
+++ src/test/scala/work/arudenko/kanban/backend/api/UserApiSpec.scala
@@ -3,6 +3,6 @@ package work.arudenko.kanban.backend.api
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
-class UserApiSpec  extends AnyFlatSpec with should.Matchers {
+class UserApiSpec extends AnyFlatSpec with should.Matchers {
 
 }
